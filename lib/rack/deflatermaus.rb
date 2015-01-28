88d820d36005f43c4f6aa126d42ee3a693b4d5e6
Gabe Berke-Williams <gabebw@gabebw.com>
2015-01-27 23:08:42 -0500

    Initial commit

---
+++ lib/rack/deflatermaus.rb
@@ -0,0 +1,7 @@
+require "rack/deflatermaus/version"
+
+module Rack
+  module Deflatermaus
+    # Your code goes here...
+  end
+end
---
+++ lib/rack/deflatermaus/version.rb
@@ -0,0 +1,5 @@
+module Rack
+  module Deflatermaus
+    VERSION = "0.0.1"
+  end
+end
